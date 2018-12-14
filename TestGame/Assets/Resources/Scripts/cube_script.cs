fdb04ad9db49cc979ea1e841f47a3fb40fc28de9
Marius <marius0903@tvk.rwth-aachen.de>
2018-12-14 18:46:41 +0100

    fixed error in class name

--- TestGame/Assets/Resources/Scripts/cube_script.cs
+++ TestGame/Assets/Resources/Scripts/cube_script.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cube_script# : MonoBehaviour {
+public class cube_script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
