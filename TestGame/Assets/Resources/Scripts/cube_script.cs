126a8f1c706bd175d4cca2249fd3f99a5bdd9662
Marius <marius0903@tvk.rwth-aachen.de>
2018-12-14 19:13:01 +0100

    silly cube move

--- TestGame/Assets/Resources/Scripts/cube_script.cs
+++ TestGame/Assets/Resources/Scripts/cube_script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class cube_script : MonoBehaviour {
 
+    public GameObject cube00;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,23 @@ public class cube_script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        moveCubeCircle();
+
+    }
+
+    void moveCubeCircle()
+    {
+        Vector3 move = new Vector3();
+        if(cube00.transform.position.x < 5)
+        {
+            move.x = cube00.transform.position.x + 1;
+        }
+        else if(cube00.transform.position.y < 5)
+        {
+            move.y = cube00.transform.position.y + 1;
+        }
+
+
+        cube00.transform.position = move;
+    }
 }
